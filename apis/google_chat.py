a312799fad159a363e568163cd2cb39fc6e5e105
Matthew H <matthew4680@icloud.com>
2024-03-27 14:55:21 -0700

    added single turn vision support for gemini

--- apis/google_chat.py
+++ apis/google_chat.py
@@ -19,27 +19,48 @@ def base64_to_image(base64_string):
     # Convert binary data to image data with BytesIO, which PIL can open
     image = Image.open(BytesIO(image_data))
     return image
-def convert_openai_to_google_messages(conversations):
-    """
-    Converts a list of conversations in OpenAI format to Google format.
 
-    OpenAI format: [{'role': 'user', 'content': 'hello'}]
-    Google format: [{'role': 'user', 'parts': ['hello']}]
+
+def format_messages(conversations, new_images=[]):
+    has_images = False
+    """
+    Converts a list of conversations in OpenAI format to Google format, including images.
+    New images to be added to the final message if present.
     """
     google_format_conversations = []
     for conversation in conversations:
+        parts = [conversation['content']] if 'content' in conversation else []
+        # Check for and convert image data to PIL Image objects, then add to parts
+        if 'image_data' in conversation:
+            for image_data in conversation['image_data']:
+                has_images = True
+                parts.append(base64_to_image(image_data['base64']))
         google_format_conversations.append({
             'role': "user" if conversation['role'] == "user" else "model",
-            'parts': [conversation['content']]
+            'parts': parts
         })
-    return google_format_conversations
+
+    # Add new images to the final message if any
+    if new_images:
+        has_images = True
+        if not google_format_conversations:
+            google_format_conversations.append({'role': 'user', 'parts': []})
+        last_message_parts = google_format_conversations[-1]['parts']
+        for image_data in new_images:
+            last_message_parts.append(base64_to_image(image_data['image']))
+
+    return google_format_conversations, has_images
 
 
 def google_complete(model, messages, images=[], max_tokens=4096, system_prompt=None):
     model_name = model_utils.get_model(model)
+
+    messages, has_images = format_messages(messages, images)
+    if has_images:
+        model_name = "gemini-pro-vision"
+
     final_model_object = genai.GenerativeModel(model_name)
 
-    messages = convert_openai_to_google_messages(messages)
     response = final_model_object.generate_content(messages,
     safety_settings={
         HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
